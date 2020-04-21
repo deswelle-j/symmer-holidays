62a4ae6b3a3f3b3b95b97f32b1d587a51eb8920c
deswelle-j <deswelle.johan@gmail.com>
2020-04-21 17:05:49 +0200

    create a advertisements list page

--- src/Controller/AdvertisementController.php
+++ src/Controller/AdvertisementController.php
@@ -2,19 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\AdvertisementRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdvertisementController extends AbstractController
 {
     /**
-     * @Route("/", name="advertisement")
+     * @Route("/", name="homepage")
      */
     public function index()
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
             
+        ]);
+    }
+
+    /**
+     * @Route("/advertisements", name="advertisement_list")
+     */
+    public function advertisements(AdvertisementRepository $adRepo)
+    {
+        $advertisements = $adRepo->findAll();
+
+        return $this->render( 'advertisement/list.html.twig', [
+            'advertisments' => $advertisements
         ]);
     }
 }
