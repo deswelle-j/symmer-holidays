26a1555e56a25486e7bf3a597b16c89af79aed20
deswelle-j <deswelle.johan@gmail.com>
2020-05-12 21:57:38 +0200

    generate RegistrationType

---
+++ src/Form/RegistrationType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('avatar')
+            ->add('username')
+            ->add('email')
+            ->add('hash')
+            ->add('introduction')
+            ->add('description')
+            ->add('slug')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
