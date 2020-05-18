c1e7647dad232c3ff2fd9622c64e2a03f5c4fbab
deswelle-j <deswelle.johan@gmail.com>
2020-05-18 21:14:14 +0200

    add persist user in database and encode password

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -7,6 +7,8 @@ use App\Form\ApplicationType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class RegistrationType extends ApplicationType
 {
