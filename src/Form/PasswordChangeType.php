fe3e2d07d84c07cb7cbea32926d9f1be484d37e9
deswelle-j <deswelle.johan@gmail.com>
2020-05-24 19:17:01 +0200

    create form in accountController and display on password template

--- src/Form/PasswordChangeType.php
+++ src/Form/PasswordChangeType.php
@@ -5,13 +5,16 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class PasswordChangeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
+            ->add('oldPassword', PasswordType::class)
+            ->add('newPassword', PasswordType::class)
+            ->add('confirmPassword', PasswordType::class)
         ;
     }
 
